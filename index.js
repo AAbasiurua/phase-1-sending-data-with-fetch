44ee87ed2b29f7b8c3b3050781aff7728dba68a7
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2025-01-11 12:52:36 -0500

    phase-1-sending-data-with-fetch-complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(username, useremail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST", 
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json",
+        },
+        body: JSON.stringify({
+            name : username,
+            email : useremail
+        })
+    })
+    .then(res  => res.json())
+    .then(object => document.body.append(object.id))
+    .catch(function (error) {
+        alert('Oh No!');
+        document.body.append(error.message);
+    })
+};
